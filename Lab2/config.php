f281688412c5a0e14bc3affed2ce42db20ca86f3
Mariam Reda <83876328+Mariam-Mokhtar@users.noreply.github.com>
2023-03-08 21:35:12 +0200

    Add files via upload

---
+++ Lab2/config.php
@@ -0,0 +1,7 @@
+<?php
+//const
+define("_THANK_YOU_","Thanks for contacting us ");
+define("_MAX_NAME_LENGTH_",100);
+define("_MAX_MSG_LENGTH_",255);
+define("_Saving_File_","../Lab2/files/data.txt")
+?>
