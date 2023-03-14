ea579af59d4e7fe862a4b1021b5b6e301d9cef25
Mariam Reda <83876328+Mariam-Mokhtar@users.noreply.github.com>
2023-03-14 21:36:31 +0200

    Delete config.php

--- Lab3/uploadFile_aws_S3/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define("AWS_KEY","AKIAXHB2C333RZ44KECK");
-define("AWS_KEY_SECRET","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");
-define("AWS_REGION","eu-north-1");
-define("AWS_BUCKET_NAME","php-iti");
-define("_MAX_SIZE_",3000000);
-define("_FILE_TYPE_","image");
-?>
